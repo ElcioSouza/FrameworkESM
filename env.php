2ab1484ede215687521e84db7baf817b67527b38
ElcioSouza <34792921+ElcioSouza@users.noreply.github.com>
2023-09-03 01:28:59 -0300

    ajuste

--- env.php
+++
@@ -1,7 +0,0 @@
-<?php
-putenv("NOMEDOBANCO_MYSQL_HOST=localhost");
-putenv("NOMEDOBANCO_MYSQL_DBNAME=loja");
-putenv("NOMEDOBANCO_MYSQL_USER=root");
-putenv("NOMEDOBANCO_MYSQL_PASSWORD=");
-putenv("NOMEDOBANCO_MYSQL_PORT=3306");
-//putenv('JWT_SECRET_KEY=');
